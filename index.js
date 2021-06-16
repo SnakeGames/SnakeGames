6041332980d4481426b806f9d56bdb23bd00f612
SnakeGames <61299588+SnakeGames@users.noreply.github.com>
2021-06-16 01:48:50 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log('Ready!')
+});
+
+bot.login('ODU0NTY4NTUyOTQ2MjA0Njgy.YMl0_A.ZRFnnirNjyYG-JCNGo0CW1U5hLs');
